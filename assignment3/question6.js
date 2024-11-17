41465bb1bb8921879a5759f3af5aab77ba4e5f39
Koalarix <koalarix96@gmail.com>
2024-11-17 15:43:27 +0800

    assignment 3
    final update

--- assignment3/question6.js
+++ assignment3/question6.js
@@ -7,3 +7,32 @@ const disneyData = {
 };
 
 // Write your code below
+
+//Convert data type
+let open = parseFloat(disneyData['open']);
+let high = parseFloat(disneyData['high']);
+let low = parseFloat(disneyData['low']);
+let close = parseFloat(disneyData['close']);
+let volume = parseFloat(disneyData['volume']);
+
+//main code
+
+if(close > open) {
+	if(volume > 100000) {
+		console.log("Strong Bullish")
+	} else {
+		console.log("Bullish");
+	}
+}
+
+if(close < open) {
+	if(volume > 10000) {
+		console.log("Strong Bearish");
+	} else {
+		console.log("Bearish");
+	}
+}
+
+if(close === open) {
+	console.log("neutral");
+}
